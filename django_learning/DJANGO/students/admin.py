52f913ac843dda708333d0f85726575fd71d2369
inv-nithin007 <nithin.krishna@innovaturelabs.com>
2025-07-25 18:55:29 +0530

    Delete django_learning/DJANGO directory

--- django_learning/DJANGO/students/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'roll_number', 'class_grade', 'status']
-    list_filter = ['status', 'class_grade']
-    search_fields = ['first_name', 'last_name', 'roll_number', 'email']
-    readonly_fields = ['user']
